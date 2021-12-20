877ed204d3fdad34ca552d6f722b380b4989ea7b
tempxla <38803749+tempxla@users.noreply.github.com>
2021-12-21 00:39:51 +0900

    Fix:

--- src/proxy_router/service/default_service.clj
+++ src/proxy_router/service/default_service.clj
@@ -6,10 +6,11 @@
 (defmethod ig/prep-key :proxy-router.service/default-service [_ options]
   (merge {:logger (ig/ref :duct/logger)} options))
 
-(defmethod ig/init-key :proxy-router.service/default-service [_ {:keys [handler logger port]}]
-  (log/log logger :report ::proxy-router-initiated)
-  {:server (tcp/start-server handler {:port port})})
+(defmethod ig/init-key :proxy-router.service/default-service [_ {:keys [logger handler port]}]
+  (log/log logger :report ::proxy-router-initiated port)
+  {:logger logger
+   :server (tcp/start-server handler {:port port})})
 
-(defmethod ig/halt-key! :proxy-router.handler/default-handler [_ {:keys [logger server]}]
+(defmethod ig/halt-key! :proxy-router.service/default-service [_ {:keys [logger server]}]
   (log/log logger :report ::proxy-router-halted)
   (.close ^java.io.Closeable server))
