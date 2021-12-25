da028564e333104849a1fb0f280a5649afdc852f
tempxla <38803749+tempxla@users.noreply.github.com>
2021-12-25 19:03:05 +0900

    Add: test of extract-host-port

--- test/proxy_router/handler/default_handler_test.clj
+++ test/proxy_router/handler/default_handler_test.clj
@@ -4,8 +4,11 @@
             [proxy-router.handler.default-handler :as default-handler]))
 
 (deftest extract-host-port-test
-  (is (= nil (default-handler/extract-host-port [])))
-  (is (= {:host "example.com" :port 8080}
-         (default-handler/extract-host-port (seq ["Date: GMT" "Host: example.com:8080" nil]))))
-  (is (= {:host "example.com" :port 80}
-         (default-handler/extract-host-port (seq ["Date: GMT" "Host: example.com" nil])))))
+  (deftest case-error
+    (is (= nil (default-handler/extract-host-port [])))
+    (is (= nil (default-handler/extract-host-port (seq ["Date: GMT"])))))
+  (deftest case-success
+    (is (= {:host "example.com" :port 8080}
+           (default-handler/extract-host-port (seq ["Date: GMT" "Host: example.com:8080" nil]))))
+    (is (= {:host "example.com" :port 80}
+           (default-handler/extract-host-port (seq ["Date: GMT" "Host: example.com" nil]))))))
